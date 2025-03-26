3ebf60119d6d67c2b69c236cef9440e13f822479
Max <max@tfq.at>
2025-03-26 21:44:44 +0100

    clean up

--- src/app/a-page/page.tsx
+++
@@ -1,6 +0,0 @@
-import type { JSX } from "react";
-import React from "react";
-
-export function APage(): JSX.Element {
-  return <div>A Page</div>;
-}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import type { JSX } from "react";
-
-export function HomePage(): JSX.Element {
-  return <div>Home Page</div>;
-}
