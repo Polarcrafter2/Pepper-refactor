9a5b2a67c1bd9ac67d35b98f589bfea06753d7f0
Max <max@tfq.at>
2025-03-26 22:19:50 +0100

    tmp switch to vite for build

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     }),
   ],
   build: {
-    outDir: resolve(__dirname, "dist"),
+    outDir: resolve(__dirname, "dist/html"),
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src", "index.html"),
