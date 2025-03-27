172b7890348b067b5accf3eab30a7d9bcf759c1b
Max <max@tfq.at>
2025-03-27 18:42:39 +0100

    debug QiSession

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,12 @@ import type { JSX } from "react";
 import React from "react";
 
 export function App(): JSX.Element {
+  const qiSession = typeof QiSession !== "undefined" && QiSession;
+  const hasQiSession = !!qiSession;
   return (
     <div>
-      Hello
+      Hello hasQiSession: {hasQiSession ? "true" : "false"}
+      QiSessionm: {JSON.stringify(qiSession)}
     </div>
   );
 }
